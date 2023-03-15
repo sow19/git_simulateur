270be977f8e564d2ceb60dd7e9b40a86754cedc2
Mariama Saoudatou Sow <sow224@c304l-d21p17.campus.unicaen.fr>
2023-03-15 12:03:07 +0100

    Cellule

--- jeu_navire/src/model/Cellule.java
+++ jeu_navire/src/model/Cellule.java
@@ -1,13 +1,50 @@
-package modele;
+package model;
+
+import javax.swing.text.Position;
 
 public class Cellule {
 	private Position position;
-	private CellState state = CellState.BLANK;
-	public Cellule(Position position, CellState state) {
-		super();
+	private CellState state;
+	private boolean isAssignedShip;
+	public Cellule(Position position) {
 		this.position = position;
+		this.state = CellState.BLANK;
+		this.isAssignedShip = false;
+		
+	}
+
+	public Cellule() {
+		this.state = CellState.BLANK;
+		this.isAssignedShip = false;
+		
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	public CellState getState() {
+		return state;
+	}
+
+	public void setState(CellState state) {
 		this.state = state;
 	}
+
+	public boolean isAssignedShip() {
+		return isAssignedShip;
+	}
+
+	public void setAssignedShip(boolean isAssignedShip) {
+		this.isAssignedShip = isAssignedShip;
+	}
+	
+
+
 	
 	
 
